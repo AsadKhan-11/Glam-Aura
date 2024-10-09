849da19bb5e6b30999ccc18d8438e945ebf39514
Asad Khan <mrasad10khan@gmail.com>
2024-10-09 23:44:23 +0500

    Working on links

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,25 +1,39 @@
 import React from "react";
 import "./Products.css";
 import data from "./Data";
+
 function Products() {
+  function highlightLink(event) {
+    event.preventDefault();
+
+    const links = document.querySelectorAll(".product-category");
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    event.target.classList.add("active");
+  }
+
   return (
     <div className="Products">
       <h2 className="Products-header">OUR PRODUCTS</h2>
       <div className="container-wrapper">
         <ul className="products-list">
-          <li className="product-category">
+          <li className="product-category active" onClick={highlightLink}>
             BODY <ion-icon name="arrow-forward-outline"></ion-icon>
           </li>
-          <li className="product-category">
-            FACE <ion-icon name="arrow-forward-outline"></ion-icon>
+          <li className="product-category" onClick={highlightLink}>
+            FACE
+            <ion-icon name="arrow-forward-outline"></ion-icon>
           </li>
-          <li className="product-category">
-            HAIR <ion-icon name="arrow-forward-outline"></ion-icon>
+          <li className="product-category" onClick={highlightLink}>
+            HAIR
+            <ion-icon name="arrow-forward-outline"></ion-icon>
           </li>
-          <li className="product-category">
+          <li className="product-category" onClick={highlightLink}>
             ACCESSORIES <ion-icon name="arrow-forward-outline"></ion-icon>
           </li>
-          <li className="product-category">
+          <li className="product-category" onClick={highlightLink}>
             SETS <ion-icon name="arrow-forward-outline"></ion-icon>
           </li>
         </ul>
